46d03c7b18367052b5409145717ff778fb87d0fa
Sadykov Arsen <LoCo10291998@gmail.com>
2025-09-14 19:02:08 +0600

    HW-83: Made hashed and deleted password for response.

--- Api/models/User.ts
+++ Api/models/User.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import {UserFields} from "../types";
 
 const Schema = mongoose.Schema;
+
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema<UserFields>({
     username: {
@@ -13,6 +16,24 @@ const UserSchema = new Schema<UserFields>({
         type: String,
         required: true,
     }
+});
+
+UserSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+    next();
+});
+
+UserSchema.set('toJSON', {
+   transform: (_doc, ret: Partial<UserFields>) => {
+        delete ret.password;
+        return ret;
+   }
 });
 
 const User = mongoose.model('User', UserSchema);
