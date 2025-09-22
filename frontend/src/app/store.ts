08b4e7a6433f14539e2c788269a3932c64546f0b
Sadykov Arsen <LoCo10291998@gmail.com>
2025-09-22 19:02:11 +0600

    HW-86: I have some troubles with connection persist.

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,42 +1,29 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import {albumsReducer} from "../features/albums/albumsSlice.ts";
 import {artistsReducer} from "../features/artists/artistsSlice.ts";
 import {tracksReducer} from "../features/tracks/tracksSlice.ts";
 import {usersReducer} from "../features/users/usersSlice.ts";
-import {
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-    persistStore
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const userPersistConfig = {
-    key: 'music:users',
-    storage,
-    whitelist: ['user'],
-};
-
-const rootReducer = combineReducers({
-    albums: albumsReducer,
-    artists: artistsReducer,
-    tracks: tracksReducer,
-    users: persistReducer(userPersistConfig, usersReducer),
-});
+// import storage from 'redux-persist/lib/storage';
+//
+// const userPersistConfig = {
+//     key: 'music:users',
+//     storage,
+//     whitelist: ['user'],
+// };
+//
+// const rootReducer = combineReducers({
+//     albums: albumsReducer,
+//     artists: artistsReducer,
+//     tracks: tracksReducer,
+//     users: persistReducer(userPersistConfig, usersReducer),
+// });
 
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
-            }
-
-        })
+    reducer: {
+        albums: albumsReducer,
+        artists: artistsReducer,
+        tracks: tracksReducer,
+        users: usersReducer,
     }
 });
 
