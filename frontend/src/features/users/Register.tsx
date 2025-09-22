58cacd3337d90b4d66f37c1a8d2f277b86df0e97
Sadykov Arsen <LoCo10291998@gmail.com>
2025-09-22 15:37:17 +0600

    HW-86: Created form for register user.

--- frontend/src/features/users/Register.tsx
+++ frontend/src/features/users/Register.tsx
@@ -1,5 +1,8 @@
 import {type ChangeEvent, type FormEvent, useState} from "react";
 import type {RegisterMutation} from "../../types";
+import {Avatar, Box, Button, Link, Stack, TextField, Typography} from "@mui/material";
+import LockOutlineIcon from '@mui/icons-material/LockOutline';
+import {NavLink} from "react-router-dom";
 
 const Register = () => {
     const [state, setState] = useState<RegisterMutation>({
@@ -17,9 +20,44 @@ const Register = () => {
     };
 
     return (
-        <div>
-            Register
-        </div>
+        <Box sx={{marginTop: 8, display: 'flex',flexDirection: 'column', alignItems: 'center'}}>
+            <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+                <LockOutlineIcon/>
+            </Avatar>
+            <Typography component={'h1'} variant={'h5'}>
+                Sign Up
+            </Typography>
+            <Box component={'form'} onSubmit={onSubmitForm} sx={{my: 3, maxWidth: '400px', width: '100%'}}>
+                <Stack spacing={2}>
+                    <TextField
+                        required
+                        label="Username"
+                        name="username"
+                        value={state.username}
+                        onChange={inputChangeHandler}
+                        autoComplete="new-username"
+                    />
+                    <TextField
+                        type="password"
+                        required
+                        label="Password"
+                        name="password"
+                        value={state.password}
+                        onChange={inputChangeHandler}
+                        autoComplete="new-password"
+                    />
+                    <Button
+                        type={'submit'}
+                        fullWidth
+                        variant="contained"
+                        sx={{mb: 2}}
+                    >
+                        Sign Up
+                    </Button>
+                </Stack>
+            </Box>
+            <Link component={NavLink} to={'/login'}>Already Have an account? Sign In</Link>
+        </Box>
     );
 };
 
